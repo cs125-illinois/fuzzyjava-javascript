dfd3feb7823a3ee291749ef62933ceaffe43f4d6
Geoffrey Challen <challen@illinois.edu>
2017-11-17 14:38:21 -0600

    Experimenting with ANTLR4.

---
+++ Makefile
@@ -0,0 +1,3 @@
+grammar: bin/antlr-4.7-complete.jar lib/FuzzyJava9.g4
+
+.PHONY: grammar
---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const fuzzyJava = require('./lib/fuzzyjava')
+const minimist = require('minimist')
+const path = require('path')
+const fs = require('fs')
+
+
+let argv = minimist(process.argv.slice(2))
+let input = path.resolve(argv._[0])
+
+let fuzzy = new fuzzyJava().parseString(fs.readFileSync(input).toString()).blockStatements().run()
